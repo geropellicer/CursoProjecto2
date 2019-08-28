88ba08eb0f7dd5141bf3fc4bd3371c7c9e15c49a
Gero <geropellicer@gmail.comm>
2019-08-28 01:07:50 -0300

    Agrego vista en detalle para el articulo

--- noticias/api/urls.py
+++ noticias/api/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from noticias.api.views import lista_articulos_crear_vista
+from noticias.api.views import (lista_articulos_crear_vista,
+                                detalles_articulo_crear_vista)
 
 urlpatterns = [
-    path("articulos/", lista_articulos_crear_vista, name="lista-articulos")
+    path("articulos/", lista_articulos_crear_vista, name="lista-articulos"),
+    path("articulos/<int:pk>", detalles_articulo_crear_vista, name="detalles-articulo"),
 ]
--- noticias/api/views.py
+++ noticias/api/views.py
@@ -18,3 +18,29 @@ def lista_articulos_crear_vista(request):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def detalles_articulo_crear_vista(request, pk):
+    try:
+        articulo = Articulo.objects.get(pk=pk)
+    except Articulo.DoesNotExist:
+        return Response({
+            "error": {
+                'codigo': 404,
+                'mensaje': 'Articulo no encontrado'
+            }}, status=status.HTTP_404_NOT_FOUND)
+    
+    if(request.method == 'GET'):
+        serializer = ArticuloSerializer(articulo)
+        return Response(serializer.data)
+    
+    elif(request.method == 'PUT'):
+        serializer = ArticuloSerializer(articulo, data=request.data)
+        if(serializer.is_valid()):
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        articulo.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
